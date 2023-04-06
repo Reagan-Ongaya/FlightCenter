8e3485edb57a5f0bf84da9eb34bffa6a5b1559d8
reaganongaya <reagan.ongaya@student.moringaschool.com>
2023-04-06 08:53:08 +0300

    Revies C

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,34 +1,47 @@
 module Api
-    module V1 
-      class ReviewsController < ApplicationController
-        protect_from_forgery with: :null_session
-        
-        def create 
-            review = Review.new[review_params]
+  module V1
+    class ReviewsController < ApiController
+      before_action :authenticate
 
-            if review.save
-                render json: ReviewSerializer.new(review).serialized_json
-            else
-                render json:{ error: review.errors.massages }, status: 422
-            end
+      # POST /api/v1/reviews
+      def create
+        review = current_user.reviews.new(review_params)
+
+        if review.save
+          render json: serializer(review)
+        else
+          render json: errors(review), status: 422
         end
+      end
 
-        def destroy
-            review = Review.find(params[:id])
+      # DELETE /api/v1/reviews/:id
+      def destroy
+        review = current_user.reviews.find(params[:id])
 
-            if review.destroy
-                head :no_content
-            else
-                render json:{ error: review.errors.massages }, status: 422
-            end
+        if review.destroy
+          head :no_content
+        else
+          render json: errors(review), status: 422
         end
+      end
 
-        private
-    
-        def review_params 
-            params.require(:review).permit(:title, :description, :score, :airline_id)
-        end
+      private
 
+      # Strong params
+      def review_params
+        params.require(:review).permit(:title, :description, :score, :airline_id)
+      end
+
+      # fast_jsonapi serializer
+      def serializer(records, options = {})
+        ReviewSerializer
+          .new(records, options)
+          .serialized_json
+      end
+
+      def errors(record)
+        { errors: record.errors.messages }
       end
     end
+  end
 end
